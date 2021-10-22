eeaeb44f6c61f8ee2ddeec8abeb48355d7edee8f
shyamsundarko <shyamsundarko@gmail.com>
2021-10-22 15:31:27 +0800

    Initial commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $name = $_POST['name'];
+    $contact =$_POST['contact'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = "shyamsundarko@gmail.com";
+    $subject = "Mail from client";
+    $body = "Client Name: ".$name ."\r\n Contact number: ".$contact . "\r\n Email: ".$email . "\r\n Message: ".$message;
+    $headers = "From: noreply@shyamsundarko.com";
+    
+    if($email != NULL){
+        mail($to, $subject, $body, $headers);
+    }
+    //redirect
+    header("Location: index.html");
+?>
